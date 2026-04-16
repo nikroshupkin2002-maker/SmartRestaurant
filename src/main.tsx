6283f01e0a86e440f13ae9c4f3301e93a3c857f7
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-16 22:39:21 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import './index.css' // Проверь, что этот файл существует в папке src
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Calculator } from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
